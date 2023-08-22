781936b8a87815eea5bce5f5214a3a225e5ff23c
Aaron Brown <worldofaaron@hotmail.com>
2023-08-22 09:07:17 -0700

    exchanger functional

--- src/currency.js
+++ src/currency.js
@@ -1,6 +1,20 @@
 export default class CurrencyExchangerCall {
-  static async getRates() {
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-    return response.json();
+  // static async getRates() {
+  //   // return (await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)).json()
+  //   const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  //   return response.json();
+  // }
+  static async convertCurrency() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      if(!response.ok) {
+        const error = `${response.status} ${response.statusText}`;
+        throw new Error(error);
+      }
+      return response.json();
+    }
+    catch(error) {
+      return error;
+    }
   }
 }  
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import CurrencyExchangerCall from './currency';
 // Business Logic
 async function getCurrencyExchange() {
   try {
-    const response = await CurrencyExchangerCall.getRates();
-    // console.log(response);
+    const response = await CurrencyExchangerCall.convertCurrency();
+    console.log(response);
     if (response.result instanceof Error){
       const errorMessage = `There was a problem with the API:  ${response.message}`;
       throw new Error(errorMessage);
@@ -22,6 +22,7 @@ async function getCurrencyExchange() {
 //UI Logic
 
 function printResults(amount, currency, conversionRate) {
+  console.log("line28",conversionRate) // problem
   document.querySelector("#results").innerText = `${amount} in USD is worth ${amount * conversionRate} in ${currency}`;
 }
 
@@ -29,14 +30,20 @@ function printError(error) {
   document.querySelector("#results").innerText = error.message;
 }
 
+
 function handleFormSubmission(event) {
   event.preventDefault();
-  let conversions = getCurrencyExchange(); //is a promise.  Async await here?  add then here?
   let usdAmount = document.querySelector("#inputAmountUSD").value;
   let targetCurrency = document.querySelector("#currencyChooser").value;
-  let conversionRate = conversions[targetCurrency];
   let results = document.querySelector("#results")
-  printResults(usdAmount, targetCurrency, conversionRate);  
+  let conversions = getCurrencyExchange(); //promise
+
+  conversions.then((res) => {
+    //res is big API response obj --res.["EUR"]
+    const rate = res[targetCurrency];
+    
+    printResults(usdAmount, targetCurrency, rate)
+  })
   results.removeAttribute("class");
 }
 
